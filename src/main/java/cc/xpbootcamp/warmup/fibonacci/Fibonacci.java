3da62b36025a0ab45281021b0a169032965fba00
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-02-15 22:59:17 +0800

    throw exception when number is out of range

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,7 +1,12 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+
 public class Fibonacci {
-    public Integer calculate(int number){
+    public Integer calculate(int number) throws Exception{
+        if(number>50 || number<1){
+            throw new Exception("number is out of range");
+        }
         if(number==1 || number==2){
             return 1;
         }
