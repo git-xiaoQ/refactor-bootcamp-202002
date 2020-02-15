cea16a6bca647ea93758d03c184210f50c5db71d
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-02-15 22:42:26 +0800

    calculate the fibonacci number when given 2

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,7 +1,5 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
-import java.util.ArrayList;
-
 public class Fibonacci {
     public Integer calculate(int number){
         return 1;
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -3,13 +3,18 @@ package cc.xpbootcamp.warmup.fibonacci;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 public class FibonacciTest {
     @Test
     public void should_return_1_when_calculate_given_number_is_1(){
         Fibonacci fibonacci = new Fibonacci();
         Integer result = fibonacci.calculate(1);
+        Integer expected = 1;
+        Assert.assertEquals(expected,result);
+    }
+    @Test
+    public void should_return_1_when_calculate_given_number_is_2(){
+        Fibonacci fibonacci = new Fibonacci();
+        Integer result = fibonacci.calculate(2);
         Integer expected = 1;
         Assert.assertEquals(expected,result);
     }
