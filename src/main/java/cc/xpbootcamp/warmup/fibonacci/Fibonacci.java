6a391f143498ce69171d66e4c5bbf6561a48cda5
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-02-15 23:12:48 +0800

    formate code

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,15 +1,13 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
-import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
-
 public class Fibonacci {
-    public Integer calculate(int number) throws Exception{
-        if(number>50 || number<1){
+    public Integer calculate(int number) throws Exception {
+        if (number > 50 || number < 1) {
             throw new Exception("number is out of range");
         }
-        if(number==1 || number==2){
+        if (number == 1 || number == 2) {
             return 1;
         }
-        return calculate(number-1)+calculate(number-2);
+        return calculate(number - 1) + calculate(number - 2);
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -5,33 +5,37 @@ import org.junit.Test;
 
 public class FibonacciTest {
     @Test
-    public void should_return_1_when_calculate_given_number_is_1() throws Exception{
+    public void should_return_1_when_calculate_given_number_is_1() throws Exception {
         Fibonacci fibonacci = new Fibonacci();
         Integer result = fibonacci.calculate(1);
         Integer expected = 1;
-        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected, result);
     }
+
     @Test
-    public void should_return_1_when_calculate_given_number_is_2()throws Exception{
+    public void should_return_1_when_calculate_given_number_is_2() throws Exception {
         Fibonacci fibonacci = new Fibonacci();
         Integer result = fibonacci.calculate(2);
         Integer expected = 1;
-        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected, result);
     }
+
     @Test
-    public void should_return_2_when_calculate_given_number_is_3()throws Exception{
+    public void should_return_2_when_calculate_given_number_is_3() throws Exception {
         Fibonacci fibonacci = new Fibonacci();
         Integer result = fibonacci.calculate(3);
         Integer expected = 2;
-        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected, result);
     }
-    @Test(expected=Exception.class)
-    public void should_return_0_when_calculate_given_number_is_51()throws Exception{
+
+    @Test(expected = Exception.class)
+    public void should_return_0_when_calculate_given_number_is_51() throws Exception {
         Fibonacci fibonacci = new Fibonacci();
         Integer result = fibonacci.calculate(51);
     }
-    @Test(expected=Exception.class)
-    public void should_return_0_when_calculate_given_number_is_string()throws Exception{
+
+    @Test(expected = Exception.class)
+    public void should_return_0_when_calculate_given_number_is_string() throws Exception {
         Fibonacci fibonacci = new Fibonacci();
         Integer result = fibonacci.calculate('a');
     }
