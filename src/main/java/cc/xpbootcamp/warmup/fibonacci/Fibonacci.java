5a914e4bca40f7457731d5fb413bc0dcc83475ee
xiaoqing.liu <xiaoqing.liu@thoughtworks.com>
2020-02-15 22:39:19 +0800

    calculate the fibonacci number when given 1

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -3,9 +3,7 @@ package cc.xpbootcamp.warmup.fibonacci;
 import java.util.ArrayList;
 
 public class Fibonacci {
-    public ArrayList<Integer> calculate(int number){
-        ArrayList<Integer> fibonaccis = new ArrayList<>();
-        fibonaccis.add(number);
-        return fibonaccis;
+    public Integer calculate(int number){
+        return 1;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -9,9 +9,8 @@ public class FibonacciTest {
     @Test
     public void should_return_1_when_calculate_given_number_is_1(){
         Fibonacci fibonacci = new Fibonacci();
-        ArrayList<Integer> result = fibonacci.calculate(1);
-        ArrayList<Integer> expected = new ArrayList<>();
-        expected.add(1);
+        Integer result = fibonacci.calculate(1);
+        Integer expected = 1;
         Assert.assertEquals(expected,result);
     }
 
